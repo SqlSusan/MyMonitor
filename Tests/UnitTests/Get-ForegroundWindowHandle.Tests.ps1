aa796f7b03b6cc82b7d6cfdc636d0511657427ff
TeflonMike <dale53@btopenworld.com>
2020-05-10 22:54:00 +0100

    annoying bug with pester losing context of mock

--- Tests/UnitTests/Get-ForegroundWindowHandle.Tests.ps1
+++ Tests/UnitTests/Get-ForegroundWindowHandle.Tests.ps1
@@ -1,6 +1,6 @@
 #$ForegroundWindowHandle
 Describe "Get-ForegroundWindowHandle" -Tags 'Unit' {
-    Context "Get-ForegroundWindowHandle" -Tags 'Unit' {
+    Context "Get-ForegroundWindowHandle" {
         it -Name "Returned results" -Test {Get-ForegroundWindowHandle | Should -Not -BeNullOrEmpty}
     } #EndContext
 }
